bf162354aaf0d2ef0b98ede85ff403417528602e
Ignacio <ignacio.urrutia@uc.cl>
2023-06-18 21:57:25 -0400

    Loaded data

--- scrapper/get_stores_data.py
+++ scrapper/get_stores_data.py
@@ -18,7 +18,7 @@ def get_info(store_name):
         'google_domain': 'google.cl',
         'gl': 'cl',
         'hl': 'es',
-        'max_page': '1',
+        'max_page': '5',
         'num': '10',
     }
 
@@ -27,24 +27,31 @@ def get_info(store_name):
     return api_result.json()
 
 drugstores = [
-    'Cruz Verde',
     'Farmacia Ahumada',
     'Farmacia Botika',
     'Salcobrand',
+    'Cruz Verde',
 ]
 
 df = pd.DataFrame(columns=['chain', 'title', 'latitude', 'longitude', 'address', 'phone'])
 
 for drugstore in drugstores:
     data = get_info(drugstore)
-    for result in data['places_results']:
-        df = df.append({
-            'chain': drugstore,
-            'title': result['title'] if 'title' in result else None,
-            'latitude': result['gps_coordinates']['latitude'] if 'latitude' in result['gps_coordinates'] else None,
-            'longitude': result['gps_coordinates']['longitude'] if 'longitude' in result['gps_coordinates'] else None,
-            'address': result['address'] if 'address' in result else None,
-            'phone': str(int(result['phone'])) if 'phone' in result else None, # No testeado
-        }, ignore_index=True)
+    try:
+        for result in data['places_results']:
+            new_row = pd.DataFrame([{
+                'chain': drugstore,
+                'title': result['title'] if 'title' in result else None,
+                'latitude': result['gps_coordinates']['latitude'] if 'latitude' in result['gps_coordinates'] else None,
+                'longitude': result['gps_coordinates']['longitude'] if 'longitude' in result['gps_coordinates'] else None,
+                'address': result['address'] if 'address' in result else None,
+                'phone': result['phone'] if 'phone' in result else None, # No testeado
+            }])
+            df = pd.concat([df, new_row], ignore_index=True)
+    except Exception as e:
+        print(e)
+        with open('error.log', 'a') as f:
+            f.write(f"Error buscando {drugstore}: {e}\n")
+        continue
 
 df.to_csv(f'{results_base_path}/drugstores.csv', index=False)
